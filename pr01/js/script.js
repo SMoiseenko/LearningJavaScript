d934e099927c20a88941caf4c02a34eb2282744a
slipknotbiocom_WORK <slipknotbiocom@gmail.com>
2020-05-29 16:27:26 +0300

    29.05.2020

--- pr01/js/script.js
+++ pr01/js/script.js
@@ -2,6 +2,7 @@ let firstFunction = function (text = "HELLO") {
   document.write("<p style='color: crimson'>" + "My First Function " + text + "</p>");
 };
 
+/*
 let a = 10;
  let b = parseInt(prompt("Enter number"));
  if (isNaN(b)){
@@ -12,6 +13,7 @@ let a = 10;
    document.writeln("<p>" + c.toString() + "</p>");
    console.log(c);
  }
+  */
  let myArray = ["One", "Two", "Three"];
  console.log(myArray);
  myArray[4] = 'Four';
@@ -74,3 +76,39 @@ console.log(calculator.subtract(15));
 
 console.log(calcTest().sum(5));
 console.log(calcTest().subtract(1));
+
+let func1 = (x=-7,y=0)=>{console.log((x+7)/(y-3))};
+func1(1, 4);
+func1();
+func1(0, 3);
+
+let userfunc = (uname, uage) =>({name: uname, age: uage});
+let vasya = userfunc("Vasya", 33);
+document.write(vasya.name + ' ;' + vasya.age);
+var dateNow = new Date();
+document.write("<p>" + dateNow.getHours() + ':'+dateNow.getMinutes()+":"+ dateNow.getSeconds());
+
+let realMan = { name:"REAL_MAN",
+                age:50,
+                "full name" : "THE_REAL_MAN",
+                display() {document.write("<p>"+ this.name + " ," + this.age + ";")},
+                "full display" : function() {console.log(this["full name"])}};
+realMan.display();
+realMan["full display"]();
+delete realMan["name"];
+console.log(realMan.name);
+
+let hasRealManName = "name" in realMan;
+console.log(hasRealManName);
+
+let hasRealManFullDisplay = realMan["full display"]!==undefined;
+console.log(hasRealManFullDisplay);
+
+let hasRealManWeight = realMan.hasOwnProperty("weight");
+console.log(hasRealManWeight);
+
+for (let key in realMan){
+  console.log(key + ":" + realMan[key]);
+}
+
+let creator = (uName, uAge)=> {return{name:uName, age:uAge, displayInfo:()=>console.log}};
