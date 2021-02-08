11d245180f6bf140c68e5032c2a3274fda3e105f
slipknotbiocom_HOME <slipknotbiocom@gmail.com>
2021-02-08 23:29:24 +0300

    08.02.2021
    Add max score;
    Max score are being saved in cookie;
    Score depend from lines that has been removed at the same time;

--- tetris/script.js
+++ tetris/script.js
@@ -1,14 +1,55 @@
-let tetrisField = document.querySelector(".tetrisField");
-let tetrisScore = document.querySelector(".tetrisScore");
+const tetrisField = document.querySelector(".tetrisField");
+const tetrisScore = document.querySelector(".tetrisScore");
+const cookieScore = 'maxScore';
+let maxScore = 0;
+let scoreGradation = [0,100,300,700,1500];
 let gameSpeed = 800;
 let score = 0;
-let activeTetro = {
+const activeTetro = {
 	shape: 0,
 	x: 0,
 	y: 0
 };
+const allFigures = 'OIZSLJT';
+const figures = {
+	O: [
+		[1, 1],
+		[1, 1]
+	],
+	I: [
+		[0, 1, 0, 0],
+		[0, 1, 0, 0],
+		[0, 1, 0, 0],
+		[0, 1, 0, 0]
+	],
+	Z: [
+		[1, 1, 0],
+		[0, 1, 1],
+		[0, 0, 0]
+	],
+	S: [
+		[0, 1, 1],
+		[1, 1, 0],
+		[0, 0, 0]
+	],
+	L: [
+		[0, 1, 0],
+		[0, 1, 0],
+		[0, 1, 1]
+	],
+	J: [
+		[0, 1, 0],
+		[0, 1, 0],
+		[1, 1, 0]
+	],
+	T: [
+		[1, 1, 1],
+		[0, 1, 0],
+		[0, 0, 0]
+	]
+};
 
-let playField = [
+const playField = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -32,7 +73,7 @@ let playField = [
 ];
 //
 function draw() {
-	let tetrisScoreInnerHTML = '<h2>You score is : ' + score + '</h2>';
+	let tetrisScoreInnerHTML = '<h2>You score is : ' + score + '</h2>' + '<h2>Max score is : ' + maxScore + '</h2>';
 	let tetrisFieldInnerHTML = '';
 
 	for (let y = 0; y < playField.length; y++) {
@@ -103,7 +144,7 @@ function fixTetro() {
 }
 //
 function checkFullLine() {
-	let flagFullLine = true;
+	let flagFullLine = true, totalLines = 0;
 	for (let y = playField.length - 1; y >= 0; y--) {
 		flagFullLine = true;
 		for (let x = 0; x < playField[y].length; x++) {
@@ -119,63 +160,43 @@ function checkFullLine() {
 				}
 			}
 			y++;
-			score++;
-			console.log('score:' + score);
+			totalLines++;
 		}
 	}
+	score += scoreGradation[totalLines];
+	saveScore();
+}
+//
+function saveScore() {
+	if (maxScore < score) {
+		maxScore = score;
+		document.cookie = cookieScore + '=' + maxScore;
+	}
+}
+function setDefaultScoreInCookie() {
+	if (!document.cookie.includes(cookieScore)) document.cookie = cookieScore + '=' + 0;
+	maxScore = parseInt(getCookie(cookieScore));
+}
+//
+function getCookie(name) {
+	let cookies, c;
+	cookies = document.cookie.split('; ');
+	for (let i = 0; i < cookies.length; i++) {
+		c = cookies[i].split('=');
+		if (c[0] == name) {
+			return c[1];
+		}
+	}
+	return "";
 }
 //
 function getNewTetro() {
-	let i = getrondomIntInclusive(1, 7);
+	let i = getrondomIntInclusive(0, 6);
+	activeTetro.shape = figures[allFigures[i]];
 	activeTetro.x = 4;
 	activeTetro.y = 0;
-	switch (i) {
-		case (1):
-			activeTetro.shape = [
-				[1],
-				[1],
-				[1],
-				[1]
-			];
-			break;
-		case (2):
-			activeTetro.shape = [
-				[1, 0, 0],
-				[1, 1, 1]
-			];
-			break;
-		case (3):
-			activeTetro.shape = [
-				[0, 0, 1],
-				[1, 1, 1]
-			];
-			break;
-		case (4):
-			activeTetro.shape = [
-				[1, 1],
-				[1, 1]
-			];
-			break;
-		case (5):
-			activeTetro.shape = [
-				[0, 1, 1],
-				[1, 1, 0]
-			];
-			break;
-		case (6):
-			activeTetro.shape = [
-				[1, 1, 0],
-				[0, 1, 1]
-			];
-			break;
-		case (7):
-			activeTetro.shape = [
-				[0, 1, 0],
-				[1, 1, 1]
-			];
-			break;
-	}
 }
+
 //
 function getrondomIntInclusive(min, max) {
 	min = Math.ceil(min);
@@ -253,6 +274,7 @@ function startGame() {
 	setTimeout(startGame, gameSpeed);
 }
 //begin is here
+setDefaultScoreInCookie();
 getNewTetro();
 addActiveTetro();
 draw();
