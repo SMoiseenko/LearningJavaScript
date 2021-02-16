f2c0463875a3aa0226476ac36fd3e28b25c56e99
slipknotbiocom_WORK <slipknotbiocom@gmail.com>
2021-02-16 16:49:57 +0300

    16.02.2021
    Snake game on canvas.
    Initial build.

---
+++ snake/js/game.js
@@ -0,0 +1,73 @@
+
+/**@type{CanvasRenderingContext2D}*/
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const ground = new Image();
+ground.src = 'img/gameField.png';
+
+const foodImg = new Image();
+foodImg.src = 'img/food.png';
+
+let box = 32;
+
+let score = 0;
+
+let dir;
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+}
+
+let snake = [];
+snake[0] = {
+    x: 7 * box,
+    y: 7 * box
+}
+
+document.addEventListener('keydown', direction);
+
+function direction(event) {
+    if (event.keyCode == 37 && dir != 'right') {
+        dir = 'left';
+    } else if (event.keyCode == 38 && dir != 'down') {
+        dir = 'up';
+    } else if (event.keyCode == 39 && dir != 'left') {
+        dir = 'right';
+    } else if (event.keyCode == 40 && dir != 'up') {
+        dir = 'down';
+    }
+}
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+    ctx.drawImage(foodImg, food.x, food.y);
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = '#008000';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+    ctx.fillStyle = 'white';
+    ctx.font = '50px Consolas';
+    ctx.fillText(score, box * 2.5, box * 1.8);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    snake.pop();
+    switch (dir) {
+        case ('left'):
+            snakeX -= box;
+            break;
+        case 'right':
+            snakeX += box;
+            break;
+        case 'up':
+            snakeY += box;
+            break;
+        case 'down':
+            snakeY -= box;
+            break;
+    }
+}
+
+let game = setInterval(drawGame, 100);
